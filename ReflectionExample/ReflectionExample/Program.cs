a4a001ab48445ebb7bc3e5aab048ba6e8f35281a
MarioBasilio08 <andres.basilio@outlook.es>
2023-07-18 01:58:09 -0600

    v1

---
+++ ReflectionExample/ReflectionExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Reflection;
+//using Test.Modelos;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        //Type typeDeveloper = typeof(Developer);
+        //Console.WriteLine(typeDeveloper.Name);
+        //Console.WriteLine(typeDeveloper.IsClass);
+        //Console.WriteLine(typeDeveloper.IsAbstract);
+        //Console.WriteLine(typeDeveloper);
+
+        //MethodInfo [] methods = typeDeveloper.GetMethods();
+        //List<Attribute> list = typeDeveloper.GetCustomAttributes().ToList();
+
+        string assemblyToLoad = ConfigurationManager.AppSettings["Assembly"];
+        string privatePath = @"D:\Trabajo\Ejercicios\Reflection\ReflectionExample\ReflectionExample\bin\Debug\net6.0\dll";
+        string assemblyPath = Path.Combine(privatePath, assemblyToLoad + ".dll");
+
+
+        Assembly assemblyLoaded = Assembly.LoadFrom(assemblyPath);
+        Type[] types = assemblyLoaded.GetTypes();
+
+        Type developerType = types.FirstOrDefault(x => x.Name.Equals("Developer"));
+
+        dynamic developerInstance = Activator.CreateInstance(developerType);
+
+        MethodInfo methodInfo = developerType.GetMethod("GetCompleteName");
+
+        Type pType = developerInstance.GetType();
+        
+        object result = pType.InvokeMember("GetGuid",
+                           BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance,
+                           null, developerInstance, null);
+
+
+
+        Console.ReadLine();
+    }
+}
---
+++ ReflectionExample/Test.Modelos/Developer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Modelos
+{
+    public class Developer
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string LastName { get; set; }
+
+        public string GetCompleteName()
+        {
+            return $"Mis datos son {Name} {LastName}";
+        }
+
+        public int GetId()
+        {
+            return Id;
+        }
+        public Guid GetGuid()
+        {
+            return Guid.NewGuid();
+        }
+    }
+}
